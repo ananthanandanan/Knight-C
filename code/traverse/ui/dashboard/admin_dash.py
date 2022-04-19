4df2e91392b07e85fb7f635d9d59e45326c70919
Akshay3960 <akshay3960@gmail.com>
2022-04-19 18:42:00 +0530

    customer screen done

--- code/traverse/ui/dashboard/admin_dash.py
+++ code/traverse/ui/dashboard/admin_dash.py
@@ -11,12 +11,13 @@ class Admin_dash:
         self.session = session
         self.admin_screen.title("Admin Dashboard")
         self.admin_screen.geometry("800x600")
+        self.admin_screen.config(bg="#f7e328")
         top_label = Label(self.admin_screen, text="Dashboard", background="black", height=3, width=125, fg="#f7e328", font='Verdana 10 bold')
         top_label.place(x=0, y=0)
 
         self.admin_screen.columnconfigure(1, weight=1, minsize=100)
         self.admin_screen.rowconfigure(1, weight=1, minsize=50)
-        frame1 = Frame( master=self.admin_screen, borderwidth=4,background="#c95353",height=200,width=200)
+        frame1 = Frame( master=self.admin_screen, relief=RAISED, borderwidth=4,background="#f7e328",height=200,width=200)
         frame1.bind('<Button-1>', self.customer_button)
         frame1.grid(row=1, column=1, padx=5, pady=5)
         frame1.pack_propagate(0)
@@ -30,7 +31,7 @@ class Admin_dash:
 
         self.admin_screen.columnconfigure(1, weight=1, minsize=100)
         self.admin_screen.rowconfigure(1, weight=1, minsize=50)
-        frame2 = Frame( master=self.admin_screen, borderwidth=4,background="#c95353",height=200,width=200)
+        frame2 = Frame( master=self.admin_screen, relief=RAISED, borderwidth=4,background="#f7e328",height=200,width=200)
         frame2.bind('<Button-1>', self.manager_button)
         frame2.grid(row=1, column=2, padx=5, pady=5)
         frame2.pack_propagate(0)
@@ -44,7 +45,7 @@ class Admin_dash:
 
         self.admin_screen.columnconfigure(2, weight=1, minsize=100)
         self.admin_screen.rowconfigure(2, weight=1, minsize=50)
-        frame3 = Frame( master=self.admin_screen, borderwidth=4,background="#c95353",height=200,width=200)
+        frame3 = Frame( master=self.admin_screen, relief=RAISED, borderwidth=4,background="#f7e328",height=200,width=200)
         frame3.bind('<Button-1>', self.agent_button)
         frame3.grid(row=2, column=1, padx=5, pady=5)
         frame3.pack_propagate(0)
@@ -58,7 +59,7 @@ class Admin_dash:
 
         self.admin_screen.columnconfigure(2, weight=1, minsize=100)
         self.admin_screen.rowconfigure(2, weight=1, minsize=50)
-        frame4 = Frame( master=self.admin_screen, borderwidth=4,background="#c95353",height=200,width=200)
+        frame4 = Frame( master=self.admin_screen, relief=RAISED, borderwidth=4,background="#f7e328",height=200,width=200)
         frame4.bind('<Button-1>', self.package_button)
         frame4.grid(row=2, column=2, padx=5, pady=5)
         frame4.pack_propagate(0)
--- code/traverse/ui/dashboard/customer.py
+++ code/traverse/ui/dashboard/customer.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+from turtle import st
 
 from traverse.db.tables.User import User
 from traverse.db.tables.Customer import Customer as cus
@@ -8,13 +10,24 @@ class Customer:
     def __init__(self, session):
         self.customer_screen = Tk()
         self.session = session
-        self.customer_screen.title("Customer Dashboard")
+        self.customer_screen.title("Customer List")
         self.customer_screen.geometry("915x600")
-        self.container_tree = Frame(self.customer_screen, width=100, height=300)
-        self.container_tree.grid(row=0, column=0)
+        self.top_frame = Frame(self.customer_screen, background="black", height=25, width=915)
+        self.top_frame.pack_propagate(0)
+        self.top_frame.grid(row=0, column=0)
+        self.top_label = Label(self.top_frame, text="Customers",background="black", fg="#f7e328", font='Verdana 10 bold')
+        self.top_label.pack()
+        # self.button_frame = Frame(self.customer_screen, height=25, width=915)
+        # self.button_frame.pack_propagate(0)
+        # self.button_frame.grid(row=1, column=2)
+        # self.add = Label(self.button_frame, text="Add", height=10, width=40, background="yellow")
+        # self.add.pack()
+        self.container_tree = Frame(self.customer_screen)
+        self.container_tree.grid(row=2, column=0)
         self.container_tree.pack_propagate(0)
         columns = ('name', 'dob', 'phno', 'address', 'email', 'username', 'password', 'age')
         self.customer_tree = ttk.Treeview(self.container_tree, columns=columns, show='headings')
+        self.style = ttk.Style()
         self.customer_tree.heading('name', text='name')
         self.customer_tree.heading('dob', text='dob')
         self.customer_tree.heading('phno', text='phno')
@@ -31,6 +44,8 @@ class Customer:
         self.customer_tree.column("username", width=100)
         self.customer_tree.column("password", width=120) 
         self.customer_tree.column("age", width=30)
+        self.customer_tree.bind("<Double-1>", self.edit_item)
+        self.customer_tree.bind("<Button-1>", self.delete_item)
         contacts = []
         result = session.query(cus).join(User).all()
         for row in result:
@@ -39,10 +54,131 @@ class Customer:
         for contact in contacts:
             self.customer_tree.insert('', END, values=contact)
         self.customer_tree.grid(row=0, column=0, sticky='nsew')
-        h = Scrollbar(self.customer_screen, orient = 'horizontal')
-        h.configure(command=self.customer_tree.xview)
-        h.grid(row=1, column=0, sticky='we', columnspan=3)
-        v = Scrollbar(self.customer_screen)
-        v.grid(row=0, column=1, sticky='ns')
-        self.customer_tree.configure(yscrollcommand=v.set, xscrollcommand=h.set)
+        # h = Scrollbar(self.customer_screen, orient = 'horizontal')
+        # h.configure(command=self.customer_tree.xview)
+        # h.grid(row=1, column=0, sticky='we', columnspan=3)
+        # v = Scrollbar(self.customer_screen)
+        # v.grid(row=0, column=1, sticky='ns')
+        # self.customer_tree.configure(yscrollcommand=v.set, xscrollcommand=h.set)
+
+        self.button_frame = Frame(self.customer_screen, height=30, width=915)
+        self.button_frame.pack_propagate(0)
+        self.button_frame.grid(row=3, column=0)
+        self.add = Button(self.button_frame, text="Add", height=50, width=8, background="#f7e328", activebackground="#f7e328", font='Verdana 10 bold', command=self.add_item)
+        self.add.pack(side=LEFT)
+        self.delete = Button(self.button_frame, text="Delete", height=50, width=8, background="#f7e328", activebackground="#f7e328", font='Verdana 10 bold', command=self.delete_item)
+        self.delete.pack(side=RIGHT)
         self.customer_screen.mainloop()
+    
+    def delete_item(self, event):
+        selected_item = self.customer_tree.focus()
+        # res = messagebox.askquestion("Delete item", "are you sure you wanna delete this item?")
+        x = self.customer_tree(selected_item, 'values')
+        print("herere",x )
+        # if res=='yes':
+        #     self.clear_all()
+
+            
+    def edit_item(self, event):
+        self.edit_screen = Tk()
+        self.edit_screen.title("Customer List")
+        self.edit_screen.geometry("500x500")
+        self.edit_screen.config(bg="#f7e328")
+
+        Label(self.edit_screen, text="Please update the details below", bg="#f7e328", width=100, height=2, font='Verdana 11 bold').pack()
+        Label(self.edit_screen, text="", bg="#f7e328").pack()
+
+        Label(self.edit_screen, text="Full name", bg="#f7e328").place(x=120,y=40) 
+        self.name = Entry(self.edit_screen)
+        self.name.place(x=220,y=40)
+        
+        Label(self.edit_screen, text="Date of Birth", bg="#f7e328").place(x=120,y=80)
+        self.dob = Entry(self.edit_screen)
+        self.dob.place(x=220,y=80)
+        
+        Label(self.edit_screen, text="Phone No.", bg="#f7e328").place(x=120,y=120)
+        self.phno = Entry(self.edit_screen)
+        self.phno.place(x=220,y=120)
+        
+        Label(self.edit_screen, text="address", bg="#f7e328").place(x=120,y=160)
+        self.address = Entry(self.edit_screen)
+        self.address.place(x=220,y=160)
+        
+        Label(self.edit_screen, text="Email", bg="#f7e328").place(x=120,y=200)
+        self.email = Entry(self.edit_screen)
+        self.email.place(x=220,y=200)
+        
+        Label(self.edit_screen, text="Username", bg="#f7e328").place(x=120,y=240)
+        self.username = Entry(self.edit_screen)
+        self.username.place(x=220,y=240)
+        
+        Label(self.edit_screen, text="Password", bg="#f7e328").place(x=120,y=280)
+        self.password = Entry(self.edit_screen, show="*")
+        self.password.place(x=220,y=280)
+        
+        Label(self.edit_screen, text="Age", bg="#f7e328").place(x=120,y=320)
+        self.age = Entry(self.edit_screen)
+        self.age.place(x=220,y=320)
+
+        Button(self.edit_screen, text="Back", width=10,height=2, bg="#f7e328", activebackground="#f7e328", command= self.edit_screen.destroy, font='Verdana 10 bold').place(x=70,y=350)
+        Button(self.edit_screen, text="Save", width=10,height=2, bg="#f7e328", activebackground="#f7e328", command= self.update_user, font='Verdana 10 bold').place(x=350,y=350)
+        self.edit_screen.mainloop()
+
+    def update_user(self):
+        selected_item = self.customer_tree.selection()[0]
+        self.customer_tree.item(selected_item, values=(self.name.get(), self.dob.get(), self.phno.get(), self.address.get(), self.email.get(), self.username.get(), self.password.get(), self.age.get()))
+        self.edit_screen.destroy()
+
+    def add_item(self):
+        self.add_screen = Tk()
+        self.add_screen.title("Customer List")
+        self.add_screen.geometry("500x500")
+        self.add_screen.config(bg="#f7e328")
+
+        Label(self.add_screen, text="Please update the details below", bg="#f7e328", width=100, height=2, font='Verdana 11 bold').pack()
+        Label(self.add_screen, text="", bg="#f7e328").pack()
+
+        Label(self.add_screen, text="Full name", bg="#f7e328").place(x=120,y=40) 
+        self.name = Entry(self.add_screen)
+        self.name.place(x=220,y=40)
+        
+        Label(self.add_screen, text="Date of Birth", bg="#f7e328").place(x=120,y=80)
+        self.dob = Entry(self.add_screen)
+        self.dob.place(x=220,y=80)
+        
+        Label(self.add_screen, text="Phone No.", bg="#f7e328").place(x=120,y=120)
+        self.phno = Entry(self.add_screen)
+        self.phno.place(x=220,y=120)
+        
+        Label(self.add_screen, text="address", bg="#f7e328").place(x=120,y=160)
+        self.address = Entry(self.add_screen)
+        self.address.place(x=220,y=160)
+        
+        Label(self.add_screen, text="Email", bg="#f7e328").place(x=120,y=200)
+        self.email = Entry(self.add_screen)
+        self.email.place(x=220,y=200)
+        
+        Label(self.add_screen, text="Username", bg="#f7e328").place(x=120,y=240)
+        self.username = Entry(self.add_screen)
+        self.username.place(x=220,y=240)
+        
+        Label(self.add_screen, text="Password", bg="#f7e328").place(x=120,y=280)
+        self.password = Entry(self.add_screen, show="*")
+        self.password.place(x=220,y=280)
+        
+        Label(self.add_screen, text="Age", bg="#f7e328").place(x=120,y=320)
+        self.age = Entry(self.add_screen)
+        self.age.place(x=220,y=320)
+
+        Button(self.add_screen, text="Back", width=10,height=2, bg="#f7e328", activebackground="#f7e328", command= self.add_screen.destroy, font='Verdana 10 bold').place(x=70,y=350)
+        Button(self.add_screen, text="Add", width=10,height=2, bg="#f7e328", activebackground="#f7e328", command= self.add_user, font='Verdana 10 bold').place(x=350,y=350)
+        self.add_screen.mainloop()
+
+    def add_user(self):
+        self.customer_tree.insert('', END,values=(self.name.get(), self.dob.get(), self.phno.get(), self.address.get(), self.email.get(), self.username.get(), self.password.get(), self.age.get()))
+        self.add_screen.destroy()
+
+    def clear_all(self):
+       for item in self.customer_tree.get_children():
+            self.customer_tree.delete(item)
+
